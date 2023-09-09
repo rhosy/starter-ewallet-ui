da143fbe336c6833e9fdfb42e96487f7c2f52733
Khoirur Rosy <khoirur.rosy@gmail.com>
2023-09-09 09:00:49 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:ewallet/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter UI EWallet',
+      theme: themeApp,
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
